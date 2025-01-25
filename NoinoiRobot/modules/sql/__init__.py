d9b891e433030778fdee876e410aacc32a65bcdf
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-26 03:39:53 +0530

    Update __init__.py

--- NoinoiRobot/modules/sql/__init__.py
+++ NoinoiRobot/modules/sql/__init__.py
@@ -1,15 +1,8 @@
-from NoinoiRobot import DB_URI
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
+from pymongo import MongoClient
 
+def start() -> MongoClient:
+    client = MongoClient(DB_URI)
+    return client
 
-def start() -> scoped_session:
-    engine = create_engine(DB_URI, client_encoding="utf8")
-    BASE.metadata.bind = engine
-    BASE.metadata.create_all(engine)
-    return scoped_session(sessionmaker(bind=engine, autoflush=False))
-
-
-BASE = declarative_base()
-SESSION = start()
+client = start()
+db = client['nobii']
