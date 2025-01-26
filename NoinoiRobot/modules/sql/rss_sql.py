95c2c0f71630541e8433320aacbce11ccdb29f44
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-26 17:27:00 +0530

    Update rss_sql.py

--- NoinoiRobot/modules/sql/rss_sql.py
+++ NoinoiRobot/modules/sql/rss_sql.py
@@ -1,16 +1,14 @@
 import threading
+from pymongo import MongoClient
 
-from NoinoiRobot.modules.sql import BASE, SESSION
-from sqlalchemy import Column, BigInteger, UnicodeText
+client = MongoClient('mongodb://localhost:27017/')
+db = client['rss_database']
+rss_collection = db['rss_feed']
+
+INSERTION_LOCK = threading.RLock()
 
 
-class RSS(BASE):
-    __tablename__ = "rss_feed"
-    id = Column(BigInteger, primary_key=True)
-    chat_id = Column(UnicodeText, nullable=False)
-    feed_link = Column(UnicodeText)
-    old_entry_link = Column(UnicodeText)
-
+class RSS:
     def __init__(self, chat_id, feed_link, old_entry_link):
         self.chat_id = chat_id
         self.feed_link = feed_link
@@ -22,59 +20,29 @@ class RSS(BASE):
         )
 
 
-RSS.__table__.create(checkfirst=True)
-INSERTION_LOCK = threading.RLock()
-
-
 def check_url_availability(tg_chat_id, tg_feed_link):
-    try:
-        return (
-            SESSION.query(RSS)
-            .filter(RSS.feed_link == tg_feed_link, RSS.chat_id == tg_chat_id)
-            .all()
-        )
-    finally:
-        SESSION.close()
+    return list(rss_collection.find({"feed_link": tg_feed_link, "chat_id": tg_chat_id}))
 
 
 def add_url(tg_chat_id, tg_feed_link, tg_old_entry_link):
     with INSERTION_LOCK:
         action = RSS(tg_chat_id, tg_feed_link, tg_old_entry_link)
-
-        SESSION.add(action)
-        SESSION.commit()
+        rss_collection.insert_one(action.__dict__)
 
 
 def remove_url(tg_chat_id, tg_feed_link):
     with INSERTION_LOCK:
-        # this loops to delete any possible duplicates for the same TG User ID, TG Chat ID and link
-        for row in check_url_availability(tg_chat_id, tg_feed_link):
-            # add the action to the DB query
-            SESSION.delete(row)
-
-        SESSION.commit()
+        rss_collection.delete_many({"chat_id": tg_chat_id, "feed_link": tg_feed_link})
 
 
 def get_urls(tg_chat_id):
-    try:
-        return SESSION.query(RSS).filter(RSS.chat_id == tg_chat_id).all()
-    finally:
-        SESSION.close()
+    return list(rss_collection.find({"chat_id": tg_chat_id}))
 
 
 def get_all():
-    try:
-        return SESSION.query(RSS).all()
-    finally:
-        SESSION.close()
+    return list(rss_collection.find())
 
 
 def update_url(row_id, new_entry_links):
     with INSERTION_LOCK:
-        row = SESSION.query(RSS).get(row_id)
-
-        # set the new old_entry_link with the latest update from the RSS Feed
-        row.old_entry_link = new_entry_links[0]
-
-        # commit the changes to the DB
-        SESSION.commit()
+        rss_collection.update_one({"_id": row_id}, {"$set": {"old_entry_link": new_entry_links[0]}})
