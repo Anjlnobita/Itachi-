984ea749c9304b93152c2d1545ed5d6e9e9be27f
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-26 22:42:49 +0530

    Update blsticker_sql.py

--- NoinoiRobot/modules/sql/blsticker_sql.py
+++ NoinoiRobot/modules/sql/blsticker_sql.py
@@ -1,12 +1,6 @@
 import threading
-
 from NoinoiRobot.modules.sql import client, nobita as STICKERS_FILTERS_COLLECTION
-
 from NoinoiRobot.modules.sql import nobita as STICKER_SETTINGS_COLLECTION
-
-
-
-
 
 STICKERS_FILTER_INSERTION_LOCK = threading.RLock()
 CHAT_STICKERS = {}
@@ -19,19 +13,12 @@ def add_to_stickers(chat_id, trigger):
             'trigger': trigger
         }
         STICKERS_FILTERS_COLLECTION.update_one(stickers_filt, {'$setOnInsert': stickers_filt}, upsert=True)
-        global CHAT_STICKERS
-        if CHAT_STICKERS.get(str(chat_id), set()) == set():
-            CHAT_STICKERS[str(chat_id)] = {trigger}
-        else:
-            CHAT_STICKERS.get(str(chat_id), set()).add(trigger)
+        CHAT_STICKERS.setdefault(str(chat_id), set()).add(trigger)
 
 def rm_from_stickers(chat_id, trigger):
     with STICKERS_FILTER_INSERTION_LOCK:
-        stickers_filt = STICKERS_FILTERS_COLLECTION.find_one({'chat_id': str(chat_id), 'trigger': trigger})
-        if stickers_filt:
-            if trigger in CHAT_STICKERS.get(str(chat_id), set()):  # sanity check
-                CHAT_STICKERS.get(str(chat_id), set()).remove(trigger)
-
+        if STICKERS_FILTERS_COLLECTION.find_one({'chat_id': str(chat_id), 'trigger': trigger}):
+            CHAT_STICKERS.get(str(chat_id), set()).discard(trigger)
             STICKERS_FILTERS_COLLECTION.delete_one({'chat_id': str(chat_id), 'trigger': trigger})
             return True
         return False
@@ -50,7 +37,6 @@ def num_stickers_filter_chats():
 
 def set_blacklist_strength(chat_id, blacklist_type, value):
     with STICKERS_FILTER_INSERTION_LOCK:
-        global CHAT_BLSTICK_BLACKLISTS
         curr_setting = STICKER_SETTINGS_COLLECTION.find_one({'chat_id': str(chat_id)})
         if not curr_setting:
             curr_setting = {
@@ -64,35 +50,25 @@ def set_blacklist_strength(chat_id, blacklist_type, value):
                 'blacklist_type': int(blacklist_type),
                 'value': str(value)
             }})
-        
+
         CHAT_BLSTICK_BLACKLISTS[str(chat_id)] = {
             "blacklist_type": int(blacklist_type),
             "value": value,
         }
 
 def get_blacklist_setting(chat_id):
-    setting = CHAT_BLSTICK_BLACKLISTS.get(str(chat_id))
-    if setting:
-        return setting["blacklist_type"], setting["value"]
-    else:
-        return 1, "0"
+    return CHAT_BLSTICK_BLACKLISTS.get(str(chat_id), {"blacklist_type": 1, "value": "0"})
 
 def __load_CHAT_STICKERS():
     global CHAT_STICKERS
-    chats = STICKERS_FILTERS_COLLECTION.distinct('chat_id')
-    for chat_id in chats:
-        CHAT_STICKERS[chat_id] = []
-    
-    all_filters = STICKERS_FILTERS_COLLECTION.find()
-    for x in all_filters:
-        CHAT_STICKERS[x['chat_id']].append(x['trigger'])
-    
-    CHAT_STICKERS = {x: set(y) for x, y in CHAT_STICKERS.items()}
+    for chat_id in STICKERS_FILTERS_COLLECTION.distinct('chat_id'):
+        CHAT_STICKERS[chat_id] = set()
+    for x in STICKERS_FILTERS_COLLECTION.find():
+        CHAT_STICKERS[x['chat_id']].add(x['trigger'])
 
 def __load_chat_stickerset_blacklists():
     global CHAT_BLSTICK_BLACKLISTS
-    chats_settings = STICKER_SETTINGS_COLLECTION.find()
-    for x in chats_settings:
+    for x in STICKER_SETTINGS_COLLECTION.find():
         CHAT_BLSTICK_BLACKLISTS[x['chat_id']] = {
             "blacklist_type": x['blacklist_type'],
             "value": x['value'],
@@ -100,8 +76,7 @@ def __load_chat_stickerset_blacklists():
 
 def migrate_chat(old_chat_id, new_chat_id):
     with STICKERS_FILTER_INSERTION_LOCK:
-        chat_filters = STICKERS_FILTERS_COLLECTION.find({'chat_id': str(old_chat_id)})
-        for filt in chat_filters:
+        for filt in STICKERS_FILTERS_COLLECTION.find({'chat_id': str(old_chat_id)}):
             STICKERS_FILTERS_COLLECTION.update_one({'_id': filt['_id']}, {'$set': {'chat_id': str(new_chat_id)}})
 
 __load_CHAT_STICKERS()
