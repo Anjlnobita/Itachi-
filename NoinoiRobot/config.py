f96d58c0a820eea117eb626035d62cc0d6cb0d78
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-26 18:54:37 +0530

    Update config.py

--- NoinoiRobot/config.py
+++ NoinoiRobot/config.py
@@ -1,4 +1,3 @@
-# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
 import json
 import os
 
@@ -8,64 +7,80 @@ def get_user_list(config, key):
         return json.load(json_file)[key]
 
 
-# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
+
 class Config(object):
     LOGGER = True
-    # REQUIRED
-    # Login to https://my.telegram.org and fill in these slots with the details given by it
+    
 
-    API_ID = 123456  # integer value, dont use "6973446"
-    API_HASH = "d3a6dbd3e466159f7170f6af7fb35ac1"
-    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "bazigaryt"
-    SUPPORT_CHAT = "cfc_bot_support"  # Your own group for support, do not add the @
+    API_ID = 20650066
+    API_HASH = "7a4f8ed638f1369a40693574c2835217"
+
+
+    TOKEN = "BOT_TOKEN"
+
+    OWNER_ID = 6777860063
+
+    OWNER_USERNAME = "anjlnobita"
+
+
+    SUPPORT_CHAT = "anime_societyy"
+
     JOIN_LOGGER = (
-        -1001253661229
-    )  # Prints any new group the bot is added to, prints just the name and ID.
-    EVENT_LOGS = (
-        -1001190806654
-    )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
+        -1002372313866
+    )
 
-    # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = "something://somewhat:user@hosturl:port/databasename"  # needed for any database modules
+    EVENT_LOGS = (
+        -1002372313866
+    ) 
+
+    
+    DB_URI = "something://somewhat:user@hosturl:port/databasename"
+
     LOAD = []
     NO_LOAD = ["rss", "cleaner", "connection", "math"]
     WEBHOOK = False
     INFOPIC = True
     URL = None
-    SPAMWATCH_API = "29sw~FuIbphLGXWd38AYb6VnlJ86DKKfnjWHS91VlpUUSyzpHoVlZWxQ0F4P_ZTy"  # go to support.spamwat.ch to get key
-    SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
 
-    # OPTIONAL
-    ##List of id's -  (not usernames) for users which have sudo access to the bot.
+    SPAMWATCH_API = "29sw~FuIbphLGXWd38AYb6VnlJ86DKKfnjWHS91VlpUUSyzpHoVlZWxQ0F4P_ZTy"  
+
+    SPAMWATCH_SUPPORT_CHAT = "@anime_societyy"
+
+   
     DRAGONS = get_user_list("elevated_users.json", "1963422158")
-    ##List of id's - (not usernames) for developers who will have the same perms as the owner
+    
     DEV_USERS = get_user_list("elevated_users.json", "1963422158")
-    ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    
     DEMONS = get_user_list("elevated_users.json", "1963422158")
-    # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    
     TIGERS = get_user_list("elevated_users.json", "1963422158")
+
     WOLVES = get_user_list("elevated_users.json", "1963422158")
-    DONATION_LINK = None  # EG, paypal
+
+    DONATION_LINK = None  
     CERT_PATH = None
     PORT = 5000
-    DEL_CMDS = True  # Delete commands that users dont have access to, like delete /ban if a non admin uses it.
+    DEL_CMDS = True
     STRICT_GBAN = True
     WORKERS = (
-        8  # Number of subthreads to use. Set as number of threads your processor uses
+        8  
     )
-    BAN_STICKER = ""  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
-    ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
+    BAN_STICKER = ""  
+    ALLOW_EXCL = True
+
     CASH_API_KEY = (
-        "V7NS1NBFEL4X24L6"  # Get your API key from https://www.alphavantage.co/support/#api-key
+        "V7NS1NBFEL4X24L6"
     )
-    TIME_API_KEY = "2AS711XS1O9B"  # Get your API key from https://timezonedb.com/api
+
+    TIME_API_KEY = "2AS711XS1O9B"
+
     WALL_API = (
-        "awoo"  # For wallpapers, get one from https://wall.alphacoders.com/api.php
+        "awoo"
     )
+
     AI_API_KEY = "awoo"  # For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
-    BL_CHATS = []  # List of groups that you want blacklisted.
+    BL_CHATS = []
+
     SPAMMERS = None
 
 class Production(Config):
