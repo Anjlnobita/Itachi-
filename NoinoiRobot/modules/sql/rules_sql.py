592397481b9e483b5c2010447406f28139f8f304
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-26 17:28:09 +0530

    Update rules_sql.py

--- NoinoiRobot/modules/sql/rules_sql.py
+++ NoinoiRobot/modules/sql/rules_sql.py
@@ -1,57 +1,46 @@
 import threading
+from pymongo import MongoClient
 
-from NoinoiRobot.modules.sql import BASE, SESSION
-from sqlalchemy import Column, String, UnicodeText, distinct, func
+client = MongoClient('mongodb://localhost:27017/')
+db = client['your_database_name']
+rules_collection = db['rules']
+
+INSERTION_LOCK = threading.RLock()
 
 
-class Rules(BASE):
-    __tablename__ = "rules"
-    chat_id = Column(String(14), primary_key=True)
-    rules = Column(UnicodeText, default="")
-
+class Rules:
     def __init__(self, chat_id):
         self.chat_id = chat_id
+        self.rules = ""
 
     def __repr__(self):
         return "<Chat {} rules: {}>".format(self.chat_id, self.rules)
 
 
-Rules.__table__.create(checkfirst=True)
-
-INSERTION_LOCK = threading.RLock()
-
-
 def set_rules(chat_id, rules_text):
     with INSERTION_LOCK:
-        rules = SESSION.query(Rules).get(str(chat_id))
+        rules = rules_collection.find_one({"chat_id": str(chat_id)})
         if not rules:
             rules = Rules(str(chat_id))
-        rules.rules = rules_text
-
-        SESSION.add(rules)
-        SESSION.commit()
+            rules_collection.insert_one({"chat_id": rules.chat_id, "rules": rules_text})
+        else:
+            rules_collection.update_one({"chat_id": str(chat_id)}, {"$set": {"rules": rules_text}})
 
 
 def get_rules(chat_id):
-    rules = SESSION.query(Rules).get(str(chat_id))
+    rules = rules_collection.find_one({"chat_id": str(chat_id)})
     ret = ""
     if rules:
-        ret = rules.rules
-
-    SESSION.close()
+        ret = rules['rules']
     return ret
 
 
 def num_chats():
-    try:
-        return SESSION.query(func.count(distinct(Rules.chat_id))).scalar()
-    finally:
-        SESSION.close()
+    return rules_collection.distinct("chat_id").count()
 
 
 def migrate_chat(old_chat_id, new_chat_id):
     with INSERTION_LOCK:
-        chat = SESSION.query(Rules).get(str(old_chat_id))
+        chat = rules_collection.find_one({"chat_id": str(old_chat_id)})
         if chat:
-            chat.chat_id = str(new_chat_id)
-        SESSION.commit()
+            rules_collection.update_one({"chat_id": str(old_chat_id)}, {"$set": {"chat_id": str(new_chat_id)}})
