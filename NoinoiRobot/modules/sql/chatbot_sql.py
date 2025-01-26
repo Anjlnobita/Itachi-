881f13275fe4d75261ab5ba6ee21654b555f393b
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-26 22:58:03 +0530

    Update chatbot_sql.py

--- NoinoiRobot/modules/sql/chatbot_sql.py
+++ NoinoiRobot/modules/sql/chatbot_sql.py
@@ -1,19 +1,11 @@
 import threading
-
 from NoinoiRobot.modules.sql import client, nobita as collection
-
-
 
 INSERTION_LOCK = threading.RLock()
 
-
 def is_chat(chat_id):
-    try:
-        chat = collection.find_one({"chat_id": str(chat_id)})
-        return chat is not None
-    finally:
-        client.close()
-
+    chat = collection.find_one({"chat_id": str(chat_id)})
+    return chat is not None
 
 def set_ses(chat_id, ses_id, expires):
     with INSERTION_LOCK:
@@ -24,28 +16,15 @@ def set_ses(chat_id, ses_id, expires):
         else:
             collection.update_one({"chat_id": str(chat_id)}, {"$set": {"ses_id": str(ses_id), "expires": str(expires)}})
 
-
 def get_ses(chat_id):
     autochat = collection.find_one({"chat_id": str(chat_id)})
-    sesh = ""
-    exp = ""
     if autochat:
-        sesh = str(autochat['ses_id'])
-        exp = str(autochat['expires'])
-
-    client.close()
-    return sesh, exp
-
+        return str(autochat['ses_id']), str(autochat['expires'])
+    return "", ""
 
 def rem_chat(chat_id):
     with INSERTION_LOCK:
-        autochat = collection.find_one({"chat_id": str(chat_id)})
-        if autochat:
-            collection.delete_one({"chat_id": str(chat_id)})
-
+        collection.delete_one({"chat_id": str(chat_id)})
 
 def get_all_chats():
-    try:
-        return [chat['chat_id'] for chat in collection.find({}, {"chat_id": 1})]
-    finally:
-        client.close()
+    return [chat['chat_id'] for chat in collection.find({}, {"chat_id": 1})]
