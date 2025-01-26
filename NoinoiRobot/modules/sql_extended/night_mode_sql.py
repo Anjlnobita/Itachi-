2d3e3ff0cf0487cfeeb9d2cf91a6ca564c1846ca
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-26 18:43:25 +0530

    Update night_mode_sql.py

--- NoinoiRobot/modules/sql_extended/night_mode_sql.py
+++ NoinoiRobot/modules/sql_extended/night_mode_sql.py
@@ -1,41 +1,26 @@
-from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
-from NoinoiRobot.modules.sql import BASE, SESSION
+from NoinoiRobot.modules.sql import client, nobita as nightmode_collection
 
 
-class Nightmode(BASE):
-    __tablename__ = "nightmode"
-    chat_id = Column(String(14), primary_key=True)
 
+
+class Nightmode:
     def __init__(self, chat_id):
         self.chat_id = chat_id
 
 
-Nightmode.__table__.create(checkfirst=True)
-
-
 def add_nightmode(chat_id: str):
     nightmoddy = Nightmode(str(chat_id))
-    SESSION.add(nightmoddy)
-    SESSION.commit()
+    nightmode_collection.insert_one(nightmoddy.__dict__)
 
 
 def rmnightmode(chat_id: str):
-    rmnightmoddy = SESSION.query(Nightmode).get(str(chat_id))
-    if rmnightmoddy:
-        SESSION.delete(rmnightmoddy)
-        SESSION.commit()
+    nightmode_collection.delete_one({'chat_id': str(chat_id)})
 
 
 def get_all_chat_id():
-    stark = SESSION.query(Nightmode).all()
-    SESSION.close()
-    return stark
+    return list(nightmode_collection.find({}, {'_id': 0, 'chat_id': 1}))
 
 
 def is_nightmode_indb(chat_id: str):
-    try:
-        s__ = SESSION.query(Nightmode).get(str(chat_id))
-        if s__:
-            return str(s__.chat_id)
-    finally:
-        SESSION.close()
+    result = nightmode_collection.find_one({'chat_id': str(chat_id)})
+    return str(result['chat_id']) if result else None
