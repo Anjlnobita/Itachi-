5063b18e3c698c2c49229b2b15c780ae07391374
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-26 22:44:55 +0530

    Update afk_sql.py

--- NoinoiRobot/modules/sql/afk_sql.py
+++ NoinoiRobot/modules/sql/afk_sql.py
@@ -1,24 +1,16 @@
-"""
-
 import threading
 
 from NoinoiRobot.modules.sql import client, nobita as afk_collection
-
-
-
 
 INSERTION_LOCK = threading.RLock()
 
 AFK_USERS = {}
 
-
 def is_afk(user_id):
     return user_id in AFK_USERS
 
-
 def check_afk_status(user_id):
     return afk_collection.find_one({"user_id": user_id})
-
 
 def set_afk(user_id, reason=""):
     with INSERTION_LOCK:
@@ -27,22 +19,17 @@ def set_afk(user_id, reason=""):
             afk_collection.insert_one({"user_id": user_id, "reason": reason, "is_afk": True})
         else:
             afk_collection.update_one({"user_id": user_id}, {"$set": {"is_afk": True}})
-
         AFK_USERS[user_id] = reason
-
 
 def rm_afk(user_id):
     with INSERTION_LOCK:
         curr = afk_collection.find_one({"user_id": user_id})
         if curr:
-            if user_id in AFK_USERS:  # sanity check
+            if user_id in AFK_USERS:
                 del AFK_USERS[user_id]
-
             afk_collection.delete_one({"user_id": user_id})
             return True
-
         return False
-
 
 def toggle_afk(user_id, reason=""):
     with INSERTION_LOCK:
@@ -54,13 +41,9 @@ def toggle_afk(user_id, reason=""):
         else:
             afk_collection.update_one({"user_id": user_id}, {"$set": {"is_afk": True}})
 
-
 def __load_afk_users():
     global AFK_USERS
     all_afk = afk_collection.find({"is_afk": True})
     AFK_USERS = {user['user_id']: user['reason'] for user in all_afk}
 
-
 __load_afk_users()
-
-"""
