2113bdbecee2983cab02ad0ce1ddd7074add4464
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-26 18:31:24 +0530

    Update global_bans_sql.py

--- NoinoiRobot/modules/sql/global_bans_sql.py
+++ NoinoiRobot/modules/sql/global_bans_sql.py
@@ -1,8 +1,7 @@
 import threading
-from pymongo import MongoClient
 
-client = MongoClient('mongodb://localhost:27017/')
-db = client['your_database_name']
+from NoinoiRobot.modules.sql import client, nobita as db
+
 
 GBANNED_USERS_LOCK = threading.RLock()
 GBAN_SETTING_LOCK = threading.RLock()
