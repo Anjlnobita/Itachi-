256c05bc6564ad7b1addd8f6e7300eb8f07c5d38
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-26 20:43:16 +0530

    Update config.py

--- NoinoiRobot/config.py
+++ NoinoiRobot/config.py
@@ -2,10 +2,6 @@ import json
 import os
 
 
-def get_user_list(config, key):
-    with open("{}/Noi/{}".format(os.getcwd(), config), "r") as json_file:
-        return json.load(json_file)[key]
-
 
 
 class Config(object):
